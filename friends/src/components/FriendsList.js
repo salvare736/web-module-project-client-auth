f1154037f974c5d64ca745313b626a25f8aab38b
Sergio Alvarez <salvare736@gmail.com>
2021-04-20 17:59:25 -0400

    Adding friend to the database works as intended

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -5,16 +5,88 @@ import { axiosWithAuth } from '../utils/axiosWithAuth';
 class FriendsList extends React.Component {
     state = {
         friendFormValues: {
-            id: NaN,
+            id: '',
             name: '',
-            age: NaN,
+            age: Number('18'),
             email: ''
+        },
+        friends: []
+    };
+
+    handleChange = (e) => {
+        if (e.target.name === 'age') {
+            this.setState({
+                friendFormValues: {
+                    ...this.state.friendFormValues,
+                    [e.target.name]: Number(e.target.value),
+                    id: Math.random()
+                },
+                friends: [...this.state.friends]
+            });
+        } else {
+            this.setState({
+                friendFormValues: {
+                    ...this.state.friendFormValues,
+                    [e.target.name]: e.target.value,
+                    id: Math.random()
+                },
+                friends: [...this.state.friends]
+            });
         }
+    };
+
+    addFriend = (e) => {
+        e.preventDefault();
+        axiosWithAuth()
+            .post('api/friends', this.state.friendFormValues)
+            .then((resp) => {
+                console.log(resp.data);
+                this.setState({
+                    friendFormValues: {
+                        ...this.state.friendFormValues
+                    },
+                    friends: resp.data
+                });
+            })
+            .catch((err) => {
+                console.log(err)
+            });
     };
 
     render() {
         return (
             <div className='FriendsList-container'>
+                <h2>Add a friend to your Friends List!</h2>
+                <br/>
+                <form onSubmit={this.addFriend}>
+                    <label>Name:</label>
+                    <input
+                        type='text'
+                        name='name'
+                        value={this.state.friendFormValues.name}
+                        onChange={this.handleChange}
+                    />
+                    <br/>
+                    <label>Age:</label>
+                    <input
+                        type='number'
+                        name='age'
+                        min='1'
+                        value={this.state.friendFormValues.age}
+                        onChange={this.handleChange}
+                    />
+                    <br/>
+                    <label>Email:</label>
+                    <input
+                        type='email'
+                        name='email'
+                        placeholder="email@gmail.com"
+                        value={this.state.friendFormValues.email}
+                        onChange={this.handleChange}
+                    />
+                    <br/>
+                    <button>Add friend</button>
+                </form>
             </div>
         );
     }
