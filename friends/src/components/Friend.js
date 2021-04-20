bcab89b6e02cf4f88771e0a5a8690309fdab6761
Sergio Alvarez <salvare736@gmail.com>
2021-04-20 18:50:35 -0400

    Achieved MVP functionality!

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -4,6 +4,9 @@ class Friend extends React.Component {
     render() {
         return (
             <div className='Friend-container'>
+                <h4>{this.props.name}</h4>
+                <p>Age: {this.props.age}</p>
+                <p>Email: {this.props.email}</p>
             </div>
         );
     }
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -10,7 +10,8 @@ class FriendsList extends React.Component {
             age: Number('18'),
             email: ''
         },
-        friends: []
+        friends: [],
+        dataLoaded: false
     };
 
     handleChange = (e) => {
@@ -19,18 +20,20 @@ class FriendsList extends React.Component {
                 friendFormValues: {
                     ...this.state.friendFormValues,
                     [e.target.name]: Number(e.target.value),
-                    id: Math.random()
+                    id: Math.random()*100
                 },
-                friends: [...this.state.friends]
+                friends: [...this.state.friends],
+                dataLoaded: true
             });
         } else {
             this.setState({
                 friendFormValues: {
                     ...this.state.friendFormValues,
                     [e.target.name]: e.target.value,
-                    id: Math.random()
+                    id: Math.random()*100
                 },
-                friends: [...this.state.friends]
+                friends: [...this.state.friends],
+                dataLoaded: true
             });
         }
     };
@@ -40,12 +43,13 @@ class FriendsList extends React.Component {
         axiosWithAuth()
             .post('api/friends', this.state.friendFormValues)
             .then((resp) => {
-                console.log(resp.data);
+                console.log(resp.data, 'the array of friends has been updated');
                 this.setState({
                     friendFormValues: {
                         ...this.state.friendFormValues
                     },
-                    friends: resp.data
+                    friends: resp.data,
+                    dataLoaded: true
                 });
             })
             .catch((err) => {
@@ -53,42 +57,76 @@ class FriendsList extends React.Component {
             });
     };
 
+    getData() {
+        axiosWithAuth()
+            .get('/api/friends')
+            .then(resp => {
+                this.setState({
+                    friendFormValues: {
+                        ...this.state.friendFormValues
+                    },
+                    friends: resp.data,
+                    dataLoaded: true
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
     render() {
-        return (
-            <div className='FriendsList-container'>
-                <h2>Add a friend to your Friends List!</h2>
-                <br/>
-                <form onSubmit={this.addFriend}>
-                    <label>Name:</label>
-                    <input
-                        type='text'
-                        name='name'
-                        value={this.state.friendFormValues.name}
-                        onChange={this.handleChange}
-                    />
-                    <br/>
-                    <label>Age:</label>
-                    <input
-                        type='number'
-                        name='age'
-                        min='1'
-                        value={this.state.friendFormValues.age}
-                        onChange={this.handleChange}
-                    />
-                    <br/>
-                    <label>Email:</label>
-                    <input
-                        type='email'
-                        name='email'
-                        placeholder="email@gmail.com"
-                        value={this.state.friendFormValues.email}
-                        onChange={this.handleChange}
-                    />
-                    <br/>
-                    <button>Add friend</button>
-                </form>
-            </div>
-        );
+        if (this.state.dataLoaded === true) {
+            return (
+                <>
+                    <div className='Form-container'>
+                        <h2>Add a friend to your Friends List!</h2>
+                        <br/>
+                        <form onSubmit={this.addFriend}>
+                            <label>Name:</label>
+                            <input
+                                type='text'
+                                name='name'
+                                value={this.state.friendFormValues.name}
+                                onChange={this.handleChange}
+                            />
+                            <br/>
+                            <label>Age:</label>
+                            <input
+                                type='number'
+                                name='age'
+                                min='1'
+                                value={this.state.friendFormValues.age}
+                                onChange={this.handleChange}
+                            />
+                            <br/>
+                            <label>Email:</label>
+                            <input
+                                type='email'
+                                name='email'
+                                placeholder="email@gmail.com"
+                                value={this.state.friendFormValues.email}
+                                onChange={this.handleChange}
+                            />
+                            <br/>
+                            <button>Add friend</button>
+                            <br/>
+                            <br/>
+                        </form>
+                    </div>
+                    <div className='FriendsList-container'>
+                        {this.state.friends.map(friend => {
+                            return <Friend key={friend.id} name={friend.name} age={friend.age} email={friend.email} />
+                        })}
+                    </div>
+                </>
+            );
+        } else {
+            return <div className='Loading-container'><h3>Loading data...</h3></div>
+        }
     }
 }
 
