8bfe75626c9ff4fb99048538e40c29f01bbf39fe
Sergio Alvarez <salvare736@gmail.com>
2021-04-20 16:51:53 -0400

    Login and token generation is working as intended

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -9,9 +9,48 @@ class Login extends React.Component {
         }
     };
 
+    handleChange = (e) => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    login = (e) => {
+        e.preventDefault();
+        axiosWithAuth()
+            .post('api/login', this.state.credentials)
+            .then((resp) => {
+                window.localStorage.setItem('token', JSON.stringify(resp.data.payload));
+                this.props.history.push('/friendslist');
+            })
+            .catch((err) => console.log(err));
+    };
+
     render() {
         return (
             <div className='Login-container'>
+                <form onSubmit={this.login}>
+                    <label>Username:</label>
+                    <input
+                        type="text"
+                        name="username"
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                    />
+                    <br/>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                    />
+                    <br/>
+                    <button>Log in</button>
+                </form>
             </div>
         );
     }
